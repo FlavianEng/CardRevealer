06568544feae656827705b2e764b92b79e132f7b
Flavian Engevin <engevinflavian@gmail.com>
2024-11-18 18:02:24 +1100

    Initial Commit

---
+++ CardRevealer/CardRevealerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardRevealerApp.swift
+//  CardRevealer
+//
+//  Created by Flavian Engevin on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CardRevealerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardRevealerTests/CardRevealerTests.swift
@@ -0,0 +1,16 @@
+//
+//  CardRevealerTests.swift
+//  CardRevealerTests
+//
+//  Created by Flavian Engevin on 18/11/2024.
+//
+
+import Testing
+
+struct CardRevealerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
